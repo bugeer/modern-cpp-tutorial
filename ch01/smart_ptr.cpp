5cae267b3c8ca585110a9dfa016cdcb9ecebfbb7
bugeer <zhangj8630@gmail.com>
2024-04-16 01:28:25 +0800

    smart_ptr

--- ch01/smart_ptr.cpp
+++ ch01/smart_ptr.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdio>
 #include <utility>
 
@@ -48,13 +49,40 @@ shape* create_shape(shape_type type) {
     }
 }
 
+class shared_count {
+public:
+    shared_count()noexcept : count_(1) {
+    }
+    void add_count() noexcept {
+        ++count_;
+    }
+    long reduce_count() noexcept {
+        return --count_;
+    }
+    long get_count() noexcept {
+        return count_;
+    }
+
+private:
+    long count_;
+};
+
 template<typename T>
 class smart_ptr {
+    template<typename U>
+    friend class smart_ptr;
+
 public:
     explicit smart_ptr(T* ptr = nullptr): ptr_(ptr) {
+        if(ptr) {
+            shared_count_ = new shared_count();
+        }
     }
     ~smart_ptr() {
-        delete ptr_;
+        if(ptr_ && !shared_count_->reduce_count()) {
+            delete ptr_;
+            delete shared_count_;
+        }
     }
 
     // smart_ptr(const smart_ptr&) = delete;
@@ -74,10 +102,42 @@ public:
         return ptr_;
     }
 
+    smart_ptr(const smart_ptr& other) {
+        ptr_ = other.ptr_;
+        if(ptr_) {
+            other.shared_count_->add_count();
+            shared_count_ = other.shared_count_;
+        }
+    }
     smart_ptr(smart_ptr&& other) {
         ptr_ = other.release();
     }
-    smart_ptr& operator=(smart_ptr rhs) {
+    template<typename U>
+    smart_ptr(const smart_ptr<U>& other) noexcept {
+        ptr_ = other.ptr_;
+        if(ptr_) {
+            other.shared_count_->add_count();
+            shared_count_ = other.shared_count_;
+        }
+    }
+    template<typename U>
+    smart_ptr(const smart_ptr<U>&& other) noexcept {
+        ptr_ = other.ptr_;
+        if(ptr_) {
+            shared_count_ = other.shared_count_;
+            other.ptr_ = nullptr;
+        }
+    }
+    template<typename U>
+    smart_ptr(const smart_ptr<U>& other, T* ptr) noexcept {
+        ptr_ = ptr;
+        if(ptr_) {
+            other.shared_count_->add_count();
+            shared_count_ = other.shared_count_;
+        }
+    }
+
+    smart_ptr& operator=(smart_ptr rhs) noexcept {
         // smart_ptr(rhs).swap(*this);
         // return *this;
         rhs.swap(*this);
@@ -88,13 +148,25 @@ public:
         ptr_ = nullptr;
         return ptr;
     }
+    T* get() {
+        return ptr_;
+    }
+    long use_count() const noexcept {
+        if(ptr_) {
+            return shared_count_->get_count();
+        } else {
+            return 0;
+        }
+    }
     void swap(smart_ptr& rhs) {
         using std::swap;
         swap(ptr_, rhs.ptr_);
+        swap(shared_count_, rhs.shared_count_);
     }
 
 private:
     T *ptr_;
+    shared_count* shared_count_;
 };
 
 class shape_wrapper {
@@ -113,6 +185,36 @@ public:
 private:
     shape* ptr_;
 };
+
+
+template <typename T>
+void swap(smart_ptr<T>& lhs, smart_ptr<T>& rhs) noexcept {
+    lhs.swap(rhs);
+}
+
+template <typename T, typename U>
+smart_ptr<T> static_pointer_cast(const smart_ptr<U>& other) noexcept {
+    T* ptr = static_cast<T*>(other.get());
+    return smart_ptr<T>(other, ptr);
+}
+
+template <typename T, typename U>
+smart_ptr<T> reinterpret_pointer_cast(const smart_ptr<U>& other) noexcept {
+    T* ptr = reinterpret_cast<T*>(other.get());
+    return smart_ptr<T>(other, ptr);
+}
+
+template <typename T, typename U>
+smart_ptr<T> const_pointer_cast(const smart_ptr<U>& other) noexcept {
+    T* ptr = const_cast<T*>(other.get());
+    return smart_ptr<T>(other, ptr);
+}
+
+template <typename T, typename U>
+smart_ptr<T> dynamic_pointer_cast(const smart_ptr<U>& other) noexcept {
+    T* ptr = dynamic_cast<T*>(other.get());
+    return smart_ptr<T>(other, ptr);
+}
 
 int main (int argc, char *argv[]) {
     smart_ptr<shape> ptr1{create_shape(shape_type::circle)};
